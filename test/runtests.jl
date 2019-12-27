c1f0c9ec1902a53afba7534cf8c57684cf502c31
MaximeBouton <maxim.bouton@gmail.com>
2019-12-27 18:30:20 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,10 @@ ped_v = 5.0 / 3.6
 ped_theta = π/2
 ped_right_side = true
 hitpoint = 0
+ped_t_collision = 1.0
+ego_y = 0.0
+ped_x = 100.0
+ped_y = 0.0
 
 
 obstacle_right_offset = -VehicleDef().width/2 - 1.0
@@ -23,12 +27,14 @@ obstacles = [obstacle_1]
 
 # unavoidable situation
 ego_v = 50.0 / 3.6
-(rec, timestep, env, ego_vehicle, sensor, sensor_observations, risk, ttc, collision_rate, brake_request, prediction, collision) = EmergencyBrakingSystem.evaluate_scenario(ego_v, ped_v, ped_theta, obstacles, hitpoint, ped_right_side);
+ego_x = ped_x - ego_v * ped_t_collision - VehicleDef().length/2;
+(scenes, timestep, env, ego_vehicle, sensor, sensor_observations, risk, ttc, collision_rate, brake_request, prediction, collision) = EmergencyBrakingSystem.evaluate_scenario(ego_x, ego_y, ego_v, ped_x, ped_y, ped_v, ped_theta, obstacles);
 @test collision[end] == true
 
 # avoidable situation
 ego_v = 30.0 / 3.6
-(rec, timestep, env, ego_vehicle, sensor, sensor_observations, risk, ttc, collision_rate, brake_request, prediction, collision) = EmergencyBrakingSystem.evaluate_scenario(ego_v, ped_v, ped_theta, obstacles, hitpoint, ped_right_side);
+ego_x = ped_x - ego_v * ped_t_collision - VehicleDef().length/2;
+(scenes, timestep, env, ego_vehicle, sensor, sensor_observations, risk, ttc, collision_rate, brake_request, prediction, collision) = EmergencyBrakingSystem.evaluate_scenario(ego_x, ego_y, ego_v, ped_x, ped_y, ped_v, ped_theta, obstacles);
 @test collision[end] == false
 
 end
